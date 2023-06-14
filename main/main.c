e8c9862ae6861eaa08421adcd01f5f1097846c79
renbingcheng <976020612@qq.com>
2023-06-14 18:05:38 +0800

    second_commit

--- main/main.c
+++ main/main.c
@@ -29,6 +29,7 @@
 #include "protocol_examples_common.h"
 
 #define EXAMPLE_ESP_WIFI_SSID      "WIFI_CONFIG"
+//#define EXAMPLE_ESP_WIFI_PASS      "12345678"
 #define EXAMPLE_ESP_WIFI_PASS      "12345678"
 #define EXAMPLE_ESP_WIFI_CHANNEL   1
 #define EXAMPLE_MAX_STA_CONN       4
